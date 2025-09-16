3f86bcaba6e3ee1bfaabe13b5b4f078170890b7c
antgiant <antgiant@gmail.com>
2025-09-16 13:51:35 -0400

    Bug Fix: Missed a compass unlisten fix

--- js/compass.js
+++ js/compass.js
@@ -71,7 +71,7 @@ function updateCompassStatus() {
         window.removeEventListener("click", firstClick);
         window.removeEventListener("touchend", firstClick);
         window.removeEventListener("deviceorientationabsolute", onHeadingChange, true);
-        window.removeEventListener("deviceorientation", onHeadingChange);
+        window.removeEventListener("deviceorientation", onHeadingChange, true);
         conf.location_heading = prompt("Please enter your compass heading", conf.location_heading) || conf.location_heading;
         directionDiv.textContent = conf.location_heading + "°";
     }
