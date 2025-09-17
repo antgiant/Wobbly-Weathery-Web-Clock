0ddae3f4d9169b55de1ce70d3476040ca4eaffc4
antgiant <antgiant@gmail.com>
2025-09-17 13:29:12 -0400

    Adding dynamic favicon

--- js/horizon/horizon.js
+++ js/horizon/horizon.js
@@ -2,6 +2,30 @@ import renderGradient from "./gradient.js";
 import { getPosition } from "./suncalc/suncalc.js";
 import { conf } from '../config.js';
 
+function setFaviconFromGradient(top, bottom) {
+  const canvas = document.createElement("canvas");
+  canvas.width = 64;
+  canvas.height = 64;
+  const ctx = canvas.getContext("2d");
+
+  const grad = ctx.createLinearGradient(0, 0, 64, 64);
+  grad.addColorStop(0, top);
+  grad.addColorStop(1, bottom);
+  ctx.fillStyle = grad;
+  ctx.fillRect(0, 0, 64, 64);
+
+  // Update favicon
+  // Remove old favicons
+  const links = document.querySelectorAll("link[rel~='icon']");
+  links.forEach(link => link.parentNode.removeChild(link));
+
+  //add new favicon
+  const link = document.querySelector("link[rel~='icon']") || document.createElement("link");
+  link.rel = "icon";
+  link.href = canvas.toDataURL("image/png");
+  document.head.appendChild(link);
+}
+
 export function refreshSky() {
   const now = new Date();
   const sunPos = getPosition(
@@ -16,4 +40,5 @@ export function refreshSky() {
   const bottom = `rgb(${bottomVec[0]}, ${bottomVec[1]}, ${bottomVec[2]})`;
   
   document.body.style.background = gradient;
+  setFaviconFromGradient(top, bottom);
 }
